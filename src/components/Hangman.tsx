9f6e854bf132c895d01b09b8ca26fcbcd857e37f
NadmorskaAlpaka <grzesio26500@gmail.com>
2023-11-12 18:49:10 +0100

    Hangman drawing

--- src/components/Hangman.tsx
+++ src/components/Hangman.tsx
@@ -1,8 +1,21 @@
+import { HEAD,
+    LEFT_LEG,
+    RIGHT_LEG,
+    LEFT_ARM,
+    RIGHT_ARM,
+    BODY
+} from "../utils/bodyparts";
+
 const Hangman = () => {
     return (
-        <>
-            Hangman
-        </>
+        <div className="hangman">
+            {HEAD}
+            <div className="hangman-rope"></div>
+            <div className="hangman-support-bar"></div>
+            <div className="hangman-bar"></div>
+            <div className="hangman-wall"></div>
+            <div className="hangman-floor"></div>
+        </div>
     )
 }
 
---
+++ src/utils/bodyparts.tsx
@@ -0,0 +1,6 @@
+export const HEAD = (<div className="hangman-head"></div>);
+export const LEFT_LEG = (<div className="hangman-leg-left"></div>)
+export const RIGHT_LEG = (<div className="hangman-leg-right"></div>)
+export const LEFT_ARM = (<div className="hangman-arm-left"></div>)
+export const RIGHT_ARM = (<div className="hangman-arm-right"></div>)
+export const BODY = (<div className="hangman-body"></div>)
